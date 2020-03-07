b399edc9006b3374665f4b9cb1edc7e94def1300
Toby <tobycook@enucs.org.uk>
2020-03-07 12:22:14 +0000

    Added dao crud operations for tickets

--- src/main/java/dev/tobycook/eventize/dao/TicketsDAO.java
+++ src/main/java/dev/tobycook/eventize/dao/TicketsDAO.java
@@ -14,4 +14,35 @@ public interface TicketsDAO {
      * @return the all tickets
      */
     List<Ticket> getAllTickets();
+
+    /**
+     * Gets ticket by id.
+     *
+     * @param ticketId the ticket id
+     * @return the ticket by id
+     */
+    Ticket getTicketById(final Long ticketId);
+
+    /**
+     * Create ticket ticket.
+     *
+     * @param ticket the ticket
+     * @return the ticket
+     */
+    Ticket createTicket(final Ticket ticket);
+
+    /**
+     * Update ticket ticket.
+     *
+     * @param ticket the ticket
+     * @return the ticket
+     */
+    Ticket updateTicket(final Ticket ticket);
+
+    /**
+     * Delete ticket.
+     *
+     * @param ticketId the ticket id
+     */
+    void deleteTicket(final Long ticketId);
 }
--- src/main/java/dev/tobycook/eventize/dao/impl/TicketsDAOImpl.java
+++ src/main/java/dev/tobycook/eventize/dao/impl/TicketsDAOImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.List;
@@ -44,6 +45,72 @@ public class TicketsDAOImpl extends HibernateDaoSupport implements TicketsDAO {
         } catch (DataAccessException e) {
             LOGGER.error("Failed to fetch list of tickets", e);
             return Collections.emptyList();
+        }
+    }
+
+    @Override
+    public Ticket getTicketById(final Long ticketId) {
+        try {
+            return Objects.requireNonNull(getHibernateTemplate())
+                    .execute((final Session session) -> {
+                        LOGGER.info(String.format("Fetching ticket with ID: %d", ticketId));
+                        return (Ticket) session.createQuery("FROM Ticket WHERE id = :ticketId")
+                                .setParameter("ticketId", ticketId)
+                                .uniqueResult();
+                    });
+        } catch (DataAccessException e) {
+            LOGGER.error(String.format("Failed to fetch ticket with ID: %d", ticketId));
+            return null;
+        }
+    }
+
+    @Override
+    @Transactional
+    public Ticket createTicket(final Ticket ticket) {
+        try {
+            return Objects.requireNonNull(getHibernateTemplate())
+                    .execute((final Session session) -> {
+                        LOGGER.info("Creating ticket...");
+                        session.save(ticket);
+                        return ticket;
+                    });
+        } catch (DataAccessException e) {
+            LOGGER.error("Failed to create ticket", e);
+            return null;
+        }
+    }
+
+    @Override
+    @Transactional
+    public Ticket updateTicket(final Ticket ticket) {
+        try {
+            return Objects.requireNonNull(getHibernateTemplate())
+                    .execute((final Session session) -> {
+                        LOGGER.info("Updating ticket...");
+                        session.update(ticket);
+                        return ticket;
+                    });
+        } catch (DataAccessException e) {
+            LOGGER.error("Failed to update ticket", e);
+            return null;
+        }
+    }
+
+    @Override
+    @Transactional
+    public void deleteTicket(final Long ticketId) {
+        try {
+            Ticket ticket = getTicketById(ticketId);
+            if (ticket == null)
+                throw new NullPointerException("Failed to find ticket");
+            Objects.requireNonNull(getHibernateTemplate())
+                    .execute((final Session session) -> {
+                        LOGGER.info("Deleting ticket...");
+                        session.delete(ticket);
+                        return 0;
+                    });
+        } catch (DataAccessException e) {
+            LOGGER.error("Failed to delete ticket", e);
         }
     }
 }
