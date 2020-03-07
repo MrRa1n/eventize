9a576077a2e65936b201f462fcab1aa8e1160885
Toby <tobycook@enucs.org.uk>
2020-03-07 12:45:18 +0000

    Added methods to ticket service

--- src/main/java/dev/tobycook/eventize/service/TicketService.java
+++ src/main/java/dev/tobycook/eventize/service/TicketService.java
@@ -34,4 +34,47 @@ public class TicketService {
     public List<Ticket> getAllTickets() {
         return ticketsDAO.getAllTickets();
     }
+
+    /**
+     * Gets ticket by id.
+     *
+     * @param ticketId the ticket id
+     * @return the ticket by id
+     */
+    public Ticket getTicketById(final Long ticketId) {
+        return ticketsDAO.getTicketById(ticketId);
+    }
+
+    /**
+     * Create ticket ticket.
+     *
+     * @param ticket the ticket
+     * @return the ticket
+     */
+    public Ticket createTicket(final Ticket ticket) {
+        if (ticket == null)
+            throw new IllegalArgumentException("Ticket cannot be null");
+        return ticketsDAO.createTicket(ticket);
+    }
+
+    /**
+     * Update ticket ticket.
+     *
+     * @param ticket the ticket
+     * @return the ticket
+     */
+    public Ticket updateTicket(final Ticket ticket) {
+        if (ticket == null)
+            throw new IllegalArgumentException("Ticket cannot be null");
+        return ticketsDAO.updateTicket(ticket);
+    }
+
+    /**
+     * Delete ticket.
+     *
+     * @param ticketId the ticket id
+     */
+    public void deleteTicket(final Long ticketId) {
+        ticketsDAO.deleteTicket(ticketId);
+    }
 }
