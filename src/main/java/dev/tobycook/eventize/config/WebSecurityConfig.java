906db5f54cdafd93da82ac94ab89dabca58137ae
Toby <tobycook@enucs.org.uk>
2020-06-23 22:32:58 +0100

    Added comments, matchers for register, Bcrypt encoding

--- src/main/java/dev/tobycook/eventize/config/WebSecurityConfig.java
+++ src/main/java/dev/tobycook/eventize/config/WebSecurityConfig.java
@@ -12,42 +12,78 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/**
+ * The type Web security config.
+ */
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /* The Jwt Request Filter. */
     private JwtRequestFilter jwtRequestFilter;
+
+    /* The user details service. */
     private AppUserDetailsService userDetailsService;
 
+    /**
+     * Instantiates a new Web security config.
+     *
+     * @param jwtRequestFilter   the jwt request filter
+     * @param userDetailsService the user details service
+     */
     @Autowired
     public WebSecurityConfig(JwtRequestFilter jwtRequestFilter, AppUserDetailsService userDetailsService) {
         this.jwtRequestFilter = jwtRequestFilter;
         this.userDetailsService = userDetailsService;
     }
+
+    /**
+     * Configure global.
+     *
+     * @param auth the auth
+     * @throws Exception the exception
+     */
     @Autowired
     protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
+    /**
+     * Password encoder password encoder.
+     *
+     * @return the password encoder
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
-    @Override
+    /**
+     * The authentication manager bean.
+     *
+     * @return the authentication manager
+     * @throws Exception the exception
+     */
     @Bean
+    @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
+    /**
+     * Configure http security.
+     *
+     * @param httpSecurity the http security
+     * @throws Exception the exception
+     */
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.cors().disable().csrf().disable()
-                .authorizeRequests().antMatchers("/api/login").permitAll()
+                .authorizeRequests()
+                .antMatchers("/api/register", "/api/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling()
